94976ec2e77675827efd8bb6f09a64bee1ea1d13
Matvey1971 <2002abc@i.ua>
2021-10-17 20:27:22 +0300

    tasks eigth nine completed

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,24 @@
+const formElement = document.getElementsByClassName("login-form")[0];
 
+const inputEmail = formElement.elements[0];
+const inputPass = formElement.elements[1];
+
+formElement.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  const textEmail = inputEmail.value;
+  const textPassword = inputPass.value;
+
+  if (textEmail.length === 0 || textPassword === 0) {
+    alert("Все поля должны быть заполнены");
+  } else {
+    const outputObject = {
+      [inputEmail.name]: textEmail,
+      [inputPass.name]: textPassword,
+    };
+
+    console.log("📝 outputObject", outputObject);
+
+    return formElement.reset();
+  }
+});
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const buttonElement = document.querySelector(".change-color");
+const spanElement = document.querySelector(".color");
+
+buttonElement.addEventListener("click", function () {
+  let newColor = getRandomHexColor();
+
+  document.body.style = `background-color: ${newColor}`;
+
+  spanElement.textContent = newColor;
+});
